99352c0efb461c810796ee66e873f7ebcf09b605
ethanloewen <ethanloewen@gmail.com>
2022-05-16 23:21:44 -0700

    Test openai API

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,34 @@ import $ from 'jquery';
 
 export default function Form() {
 
+  const data = {
+    prompt: "Write a poem about a dog wearing skis",
+    temperature: 0.5,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+   };
+
+  // submit the text area value
   const submitText = (e) => {
     e.preventDefault();
     const text = $('#user-text').val();
     console.log(text);
+
+    fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer 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`,
+      // Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(data => console.log('data', data.choices[0].text));
+
+  //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
   };
 
   return (
