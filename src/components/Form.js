1f9d1622a05eb758b6e8da4532427fdb9c61c14d
ethanloewen <ethanloewen@gmail.com>
2022-05-16 22:54:30 -0700

    Add function for button submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,20 @@
 import './Form.scss';
+import $ from 'jquery';
 
 export default function Form() {
+
+  const submitText = (e) => {
+    e.preventDefault();
+    const text = $('#user-text').val();
+    console.log(text);
+  };
+
   return (
       <form id='user-form' action='./#'>
         <div className='wrapper'>
           <label htmlFor='ftext'>Say something:</label>
-          <textarea rows = "5" cols = "60" name = "description"></textarea>
-          <button onClick={() => console.log('tester')}>submit</button>
+          <textarea id='user-text' rows = "5" cols = "60" name = "description"></textarea>
+          <button onClick={(e) => submitText(e)}>submit</button>
         </div>
       </form>
   );
