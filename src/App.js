5053ff2020c46f55ba9b6e8e55626152b8e111a8
ethanloewen <ethanloewen@gmail.com>
2022-05-17 05:02:31 -0700

    Add latestOutput state to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
+import React, {useState} from 'react';
 import Form from './components/Form';
 import Response from './components/Response';
 
 function App() {
+  const [latestOutput, setLatestOutput] = useState('');
+
   return (
     <div className="App">
       <Form />
-      <Response />
+      <Response input={'Prompt here...'} output={'Response here...'}/>
     </div>
   );
 }
--- src/components/Response.js
+++ src/components/Response.js
@@ -1,13 +1,13 @@
 import './Response.scss';
 
-export default function Response() {
+export default function Response(props) {
 
   return (
     <div className='response-container'>
       <div className='response'>
         <div className='response-data'>
-          <h1>Prompt here...</h1>
-          <p>Response here...</p>
+          <h1>{props.input}</h1>
+          <p>{props.output}</p>
         </div>
       </div>
     </div>
